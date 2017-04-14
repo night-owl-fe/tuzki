05f8900280a591ceec75ec3086284dc047d38c52
wanglei8381 <wanglei8381@163.com>
2017-04-14 09:32:01 +0800

    feat：添加jsonp请求

--- app/controller/api.js
+++ app/controller/api.js
@@ -2,3 +2,14 @@ export default async (cxt, next) => {
   // cxt.body = 'hello'
   cxt.body = JSON.stringify({code: 2000, msg: 'ok'})
 };
+
+export async function jsonp (cxt, next) {
+  // cxt.body = 'hello'
+  console.log(cxt.query.callback)
+  let callback = cxt.query.callback
+  if (callback) {
+    cxt.body = `${callback}(${JSON.stringify({code: 2000, msg: 'ok'})})`
+  } else {
+    cxt.body = JSON.stringify({code: fail, msg: '参数缺失'})
+  }
+};
--- app/router.js
+++ app/router.js
@@ -1,9 +1,10 @@
 import Router from 'koa-router'
-import api from './controller/api'
+import api, { jsonp } from './controller/api'
 
 const router = new Router()
 
 router.get('/api', api);
+router.get('/api/jsonp', jsonp);
 
 export default router
 
